1268b60aae7bb80fdac97a7d31ce1008dbb27a12
HallSimonHall <48973539+HallSimonHall@users.noreply.github.com>
2021-02-27 12:38:00 +1100

    Modified after following tutorial

---
+++ arkanoid-ts-startHere/src/Collision.ts
@@ -0,0 +1,62 @@
+// Types
+import { Brick } from './Sprites/Brick';
+import { Paddle } from './Sprites/Paddle';
+import { Ball } from './Sprites/Ball';
+import { CanvasView } from './View/CanvasView';
+
+export class Collision {
+
+    isCollidingBrick(ball: Ball, brick: Brick): boolean {
+        if (
+            ball.pos.x < brick.pos.x + brick.width &&
+            ball.pos.x + ball.width > brick.pos.x &&
+            ball.pos.y < brick.pos.y + brick.height &&
+            ball.pos.y + ball.height > brick.pos.y
+        ) {
+            return true;
+        }
+        return false;
+
+    
+    }
+    //Check ball collision with bricks
+
+    isCollidingBricks(ball: Ball, bricks: Brick[]): boolean {
+        let colliding = false;
+
+        bricks.forEach((brick, i) => {
+            if (this.isCollidingBrick(ball, brick)) {
+ball.changeYDirection ();
+
+if (brick.energy === 1){
+    bricks.splice(i, 1);
+    } else {
+        brick.energy -= 1;
+    }
+colliding = true;
+            }
+        });
+
+        return colliding;
+    }
+
+    checkBallCollision(ball: Ball, paddle: Paddle, view: CanvasView): void {
+        // 1. Check ball collisiuon with paddle
+        if (
+            ball.pos.x + ball.width > paddle.pos.x &&
+            ball.pos.x < paddle.pos.x + paddle.width &&
+            ball.pos.y + ball.height === paddle.pos.y
+        ) {
+            ball.changeYDirection();
+        }
+        // 2. Check ball collision with walls
+        // Ball movement x constraints
+        if (ball.pos.x > view.canvas.width - ball.width || ball.pos.x < 0) {
+            ball.changeXDirection()
+        }
+        //Ball movement Y constraints
+        if (ball.pos.y < 0) {
+            ball.changeYDirection();
+        }
+    }
+}
